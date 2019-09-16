bde7a657bf83090fe80b33f818e070a667a0f4ee
An Hoang <an.hoang@go1.com>
2019-09-16 08:51:25 +0700

    GO1P-30018 Adding new manual LO types

--- lo/LoTypes.php
+++ lo/LoTypes.php
@@ -10,10 +10,11 @@ class LoTypes
     const AWARD           = 'award';
     const GROUP           = 'group';
     const ACHIEVEMENT     = 'achievement';
+    const MANUAL          = 'manual';
 
     public static function all()
     {
-        return [self::LEANING_PATHWAY, self::COURSE, self::MODULE, self::AWARD, self::GROUP, self::ACHIEVEMENT];
+        return [self::LEANING_PATHWAY, self::COURSE, self::MODULE, self::AWARD, self::GROUP, self::ACHIEVEMENT, self::MANUAL];
     }
 
     public static function allTheThing()
