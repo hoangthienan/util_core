e3133514a242d7580f89e4199a00f6fb599f52cd
Franc <franc.hakani@go1.com>
2019-10-29 09:59:36 +0800

    add quality attributes

--- lo/LoAttributes.php
+++ lo/LoAttributes.php
@@ -13,26 +13,34 @@ class LoAttributes
     /**
      * @deprecated use the REGION_RESTRICTIONS type instead
      */
-    const REGION_RESTRICTION  = 5;
-    const TOPICS              = 6;
-    const REGION_RESTRICTIONS = 7;
-    const LEARNING_OUTCOMES   = 8;
-    const PROVIDER            = 9;
-    const INTERNAL_QA_RATING  = 10;
+    const REGION_RESTRICTION            = 5;
+    const TOPICS                        = 6;
+    const REGION_RESTRICTIONS           = 7;
+    const LEARNING_OUTCOMES             = 8;
+    const PROVIDER                      = 9;
+    const INTERNAL_QA_RATING            = 10;
+    const DOWNLOAD_SPEED                = 11;
+    const AUDIO_VISUAL_DESIGN           = 12;
+    const PRESENTATION_OF_CONTENT       = 13;
+    const STRUCTURE_NAVIGATION          = 14;
 
     public static function machineName(int $attribute): ?string
     {
         $map = [
-            self::MOBILE_OPTIMISED    => 'mobile_optimised',
-            self::WCAG                => 'wcag',
-            self::ASSESSABLE          => 'assessable',
-            self::AVAILABILITY        => 'availability',
-            self::REGION_RESTRICTION  => 'region_restriction',
-            self::REGION_RESTRICTIONS => 'region_restrictions',
-            self::TOPICS              => 'topics',
-            self::LEARNING_OUTCOMES   => 'learning_outcomes',
-            self::PROVIDER            => 'provider',
-            self::INTERNAL_QA_RATING  => 'internal_qa_rating',
+            self::MOBILE_OPTIMISED          => 'mobile_optimised',
+            self::WCAG                      => 'wcag',
+            self::ASSESSABLE                => 'assessable',
+            self::AVAILABILITY              => 'availability',
+            self::REGION_RESTRICTION        => 'region_restriction',
+            self::REGION_RESTRICTIONS       => 'region_restrictions',
+            self::TOPICS                    => 'topics',
+            self::LEARNING_OUTCOMES         => 'learning_outcomes',
+            self::PROVIDER                  => 'provider',
+            self::INTERNAL_QA_RATING        => 'internal_qa_rating',
+            self::DOWNLOAD_SPEED            => 'download_speed',
+            self::AUDIO_VISUAL_DESIGN       => 'audio_visual_design',
+            self::PRESENTATION_OF_CONTENT   => 'presentation_of_content',
+            self::STRUCTURE_NAVIGATION      => 'structure_navigation'
         ];
 
         return $map[$attribute] ?? null;
