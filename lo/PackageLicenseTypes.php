ca23d909e7b956792e84ffcc4117a1becadd0bad
Peter Hartshorn <peter.hartshorn@go1.com>
2020-01-30 14:10:34 +0800

    name change of constants to better reflect what they are for

--- lo/PackageLicenseTypes.php
+++ lo/PackageLicenseTypes.php
@@ -4,11 +4,11 @@ namespace go1\util\lo;
 
 class PackageLicenseTypes
 {
-    const SEAT   = 1;
-    const PORTAL = 2;
+    const PER_SEAT   = 1;
+    const PER_PORTAL = 2;
 
     public static function all()
     {
-        return [self::SEAT, self::PORTAL];
+        return [self::PER_SEAT, self::PER_PORTAL];
     }
 }
