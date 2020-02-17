a107e70a9bef5eb6eac077b637505f911efca972
Andy Truong <thehongtt@gmail.com>
2020-02-17 16:25:38 +1000

    LO > status: Add `decommissioned`.

--- lo/LoStatuses.php
+++ lo/LoStatuses.php
@@ -4,7 +4,8 @@ namespace go1\util\lo;
 
 class LoStatuses
 {
-    const PUBLISHED   = 1;
-    const UNPUBLISHED = 0;
-    const ARCHIVED    = -1;
+    const PUBLISHED      = 1;
+    const UNPUBLISHED    = 0;
+    const ARCHIVED       = -1;
+    const DECOMMISSIONED = -2;
 }
