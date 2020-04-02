431c37fcd0a65bbaaf3af7b81305192f781e9c32
Andy Truong <thehongtt@gmail.com>
2020-04-03 09:53:16 +1000

    Add DimensionHelper::loadMultipleByTypes().

--- dimensions/DimensionHelper.php
+++ dimensions/DimensionHelper.php
@@ -19,7 +19,14 @@ class DimensionHelper
         $dimensions = !$ids ? [] : $db
             ->executeQuery('SELECT * FROM dimensions where id IN (?)', [$ids], [DB::INTEGERS])
             ->fetchAll(DB::OBJ);
+
         return $dimensions;
+    }
+
+    public static function loadMultipleByTypes(Connection $db, array $types) {
+        return $db
+            ->executeQuery('SELECT * FROM dimensions where type IN (?)', [$types], [DB::INTEGERS])
+            ->fetchAll(DB::OBJ);
     }
 
     public static function loadAllForType(Connection $db, int $type)
@@ -32,10 +39,11 @@ class DimensionHelper
     public static function loadAllForLevel(Connection $db, $level)
     {
         $level = intval($level);
-        $validLevels = [1,2,3];
+        $validLevels = [1, 2, 3];
         if (!in_array($level, $validLevels)) {
             return;
         }
+
         return $db
             ->executeQuery('SELECT *  FROM dimensions WHERE id in (select Level'.$level.' from dimensions_levels)')
             ->fetchAll(DB::OBJ);
@@ -44,7 +52,7 @@ class DimensionHelper
     public static function loadAllForLevelAndType(Connection $db, $level, $type)
     {
         $level = intval($level);
-        $validLevels = [1,2,3];
+        $validLevels = [1, 2, 3];
         if (!in_array($level, $validLevels)) {
             return;
         }
